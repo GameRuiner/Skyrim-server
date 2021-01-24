27a4398177c05fad0a971a24eb0251f45bc937ff
alekcey0211 <alekcey_ezh@mail.ru>
2021-01-25 00:37:49 +0300

    убрал не работающее

--- src/properties/messageIdToShow.ts
+++ src/properties/messageIdToShow.ts
@@ -2,11 +2,4 @@ import { MP } from '../types';
 
 declare const mp: MP;
 
-export const initMessageIdToShow = () => {
-	mp.makeProperty('messageIdToShow', {
-		isVisibleByOwner: false,
-		isVisibleByNeighbors: false,
-		updateNeighbor: '',
-		updateOwner: '',
-	});
-};
+export const initMessageIdToShow = () => {};
--- src/systems/messageSystem.ts
+++ src/systems/messageSystem.ts
@@ -3,15 +3,4 @@ import { utils } from '../utility';
 
 declare const mp: MP;
 
-export const messageSystem = {
-	/**
-	 * Show
-	 * @param formId Actor Id
-	 * @param msgFormId message form id
-	 */
-	show: (formId: number, msgFormId: number) => {
-		mp.set(formId, 'messageIdToShow', msgFormId);
-		utils.log('[MESSAGE] set', msgFormId);
-		utils.log('[MESSAGE] get', mp.get(formId, 'messageIdToShow'));
-	},
-};
+export const messageSystem = {};
